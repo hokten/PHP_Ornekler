a97756e55e64d4f8428b52702b2eb1a62c920ece
hokten <hokten@gmail.com>
2019-12-18 11:46:24 +0200

    Ders listeleme birinci sayfa eklendi

---
+++ OgrenciProgram/derslistele.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ders Listeleme Sayfası</title>
+</head>
+<body>
+<?php
+require 'veritabani.php';
+$sql = "SELECT * FROM programlar";
+$sorgu = $vt->query($sql, PDO::FETCH_ASSOC);
+$kayitlar = $sorgu->fetchAll();
+
+
+echo "<table border='1'>";
+echo "<tr><th>Program ID</th><th>Program Adı</th></tr>";
+foreach($kayitlar as $kayit) {
+    $ders_id = $kayit['program_id'];
+    echo "<tr>";
+    echo "<td>{$kayit['program_id']}</td>";
+    echo "<td><a href='derslistele1.php?progid=$ders_id'>{$kayit['program_adi']}</a></td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+?>
+</body>
+</html>
